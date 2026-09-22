3defaf13d72bd9ba69da14b89bfc119b995eee9e
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-09-23 00:02:38 +0600

    feat: implement Redux for user authentication and data management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react"
 import Home from "./pages/Home"
 import getCurrentUser from "./features/getCurrentUser"
+import { useDispatch } from "react-redux"
+import { setUserData } from "./redux/userSlice"
 
 
 const App = () => {
+  const dispatch=useDispatch();
   useEffect(()=>{
     const getUser=async()=>{
-      return await getCurrentUser();
+      const data=await getCurrentUser();
+      dispatch(setUserData(data))
     }
       getUser();
   },[])
--- frontend/src/features/getCurrentUser.js
+++ frontend/src/features/getCurrentUser.js
@@ -3,10 +3,11 @@ import api from "../../utils/axios"
 const getCurrentUser= async()=>{
     try {
         const {data}=await api.get("/api/me");
-        console.log(data);
         return data;
+     
     } catch (error) {
         console.log(error);
+        return null;
     }
 }
 
