ecdc3347060b542721cb5e1c8c571c5bf098f02b
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-09-24 09:03:15 +0600

    feat: add database connection setup and update API routes for agent service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -21,9 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth",proxy(process.env.AUTH_SERVICE_URL))
 
 app.get("/api/me",protect,getCurrentUser)
+app.use("/api/agent",protect,proxy(process.env.AGENT_SERVICE_URL))
+
 
 // Protected microservices routes
 app.use("/api/chat",protect,proxyWithHeader(process.env.CHAT_SERVICE_URL))
+
 app.get("/",(req,res)=>{
     res.status(200).json({message:"Hello from api Gateway"});
 })
