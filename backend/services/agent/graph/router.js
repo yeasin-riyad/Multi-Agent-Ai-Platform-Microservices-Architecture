10373860f6e55ba8f730b02adc8ea3708a1d2917
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-09-24 20:00:08 +0600

    feat: enhance agent router prompt with detailed agent rules and descriptions

--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,7 +1,69 @@
-import { getModel } from "../config/llmModels.js"
+import { getModel } from "../config/llmModels.js";
 
-export const router=async (params) => {
-    const llm = await getModel("router");
+export const router = async (state) => {
+  const llm = await getModel("router");
+  const prompt = `You are an agent router.
+    Available agents:
+    -chat
+    -search
+    -coding
+    -pdf
+    -ppt
+    -vision
 
+    Rules:
+
+    chat:
+    General conversation,
+    explanations,
+    learning,
+    questions.
+
+    search:
+    Current events,
+    latest information,
+    news,
+    recent developments,
+    internet lookup.
+
+    coding:
+    Generate code,
+    debug code,
+    build projects,
+    architecture,
+    API design.
+
+    pdf:
+    Questions about generate PDFs
+    or document context.
+
+    ppt:
+    Questions about generate ppts
+    or ppt context.
+
+    vision:
+    Generate image,
+    Create image,
+    Edit image
+
+
+    Return only one word:
+
+    chat 
+    search
+    coding 
+    pdf
+    ppt
+    vision
+
+    User Query:
+    ${state.prompt}   
     
-}
+    `;
+
+  const response = await llm.invoke(prompt);
+  return {
+    ...state,
+    agent: response.content.trim().toLowerCase(),
+  };
+};
